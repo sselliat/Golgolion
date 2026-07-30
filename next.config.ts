12f5d62e2a84b19a290c3def32e419c9cdf13a72
lowiq404brain <artanother@gmail.com>
2026-07-31 04:39:45 +0900

    test(ci): 실패 검증용 build 오류 제거

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from 'next';
 
-throw new Error('Intentional CI build failure');
-
 const nextConfig: NextConfig = {/* config options here */};
 
 export default nextConfig;
